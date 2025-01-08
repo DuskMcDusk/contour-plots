0f6470257d0ea0425a0c33426f4a0c0d3fa4d8bb
DuskMcDusk <schadenfreudewelle@gmail.com>
2025-01-08 21:33:34 +0100

    x data transformation

---
+++ main.py
@@ -0,0 +1,23 @@
+from math import asin, degrees, radians, sin
+
+
+testPath = "Data/Tests/test.txt"
+expectedPath = "Data/Tests/expected.txt"
+testData = []
+
+def ConvertX(value):
+    M = asin((1.54/0.413) * (sin(radians(value))/2))
+    return degrees(2*M)
+
+with open(testPath) as f:
+    file_contents = f.read()
+    records = file_contents.split("\n")
+    entries = []
+    for row in records:
+        columns = row.split()
+        entries.append((float(columns[0]), float(columns[1])))
+    print("entries\n", entries)
+    for entry in entries:
+        xColumn = ConvertX(entry[0])
+        testData.append(xColumn)    
+    print("\nDATA:\n",testData)
